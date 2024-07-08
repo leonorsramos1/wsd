36137adbcff9f05bfe5d93e1940b42a3b99fedaf
leonorsramos1 <leonor.s.ramos@gmail.com>
2024-07-08 17:54:08 +0100

    commit

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
+import { Eta } from "https://deno.land/x/eta@v3.4.0/src/index.ts";
 import { Hono } from "https://deno.land/x/hono@v3.12.11/mod.ts";
 import * as feedbacks from "./feedbacks.js";
-import { Eta } from "https://deno.land/x/eta@v3.4.0/src/index.ts";
 
 const eta = new Eta({ views: `${Deno.cwd()}/templates/` });
 const app = new Hono();
+
+app.get("/", async (c) => {
+  return c.html(eta.render("index.eta"));
+});
 
 app.get("/feedbacks/:id", async (c) => {
   const id = c.req.param("id");
@@ -17,12 +21,4 @@ app.post("/feedbacks/:id", async (c) => {
   return c.redirect("/");
 });
 
-
-// Root path to display rating buttons using index.eta template
-app.get("/", (c) => {
-    return c.html(
-      eta.render("index.eta", {})
-    );
-  });
-  
-  export default app;
+export default app;
