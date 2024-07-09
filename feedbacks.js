7cdbb7e2cfb6f0e9bfe803cf245141bf7610ea43
leonorsramos1 <leonor.s.ramos@gmail.com>
2024-07-09 09:12:25 +0100

    commit

--- feedbacks.js
+++ feedbacks.js
@@ -1,13 +1,17 @@
+import { Eta } from "https://deno.land/x/eta@v3.4.0/src/index.ts";
+const eta = new Eta({ views: `${Deno.cwd()}/templates/` });
+
 const getFeedbackCount = async (id) => {
-  const kv = await Deno.openKv();
-  const store = await kv.get(["feedbacks", id]);
-  return store?.value ?? 0;
-};
-
-const incrementFeedbackCount = async (id) => {
-  const kv = await Deno.openKv();
-  const count = await getFeedbackCount(id);
-  await kv.set(["feedbacks", id], count + 1);
-};
-
-export { getFeedbackCount, incrementFeedbackCount };
+    const kv = await Deno.openKv();
+    const store = await kv.get(["feedbacks", id]);
+    return store?.value ?? 0;
+  };
+  
+  const incrementFeedbackCount = async (id) => {
+    const kv = await Deno.openKv();
+    const count = await getFeedbackCount(id);
+    await kv.set(["feedbacks", id], count + 1);
+  };
+  
+  export { getFeedbackCount, incrementFeedbackCount };
+  
